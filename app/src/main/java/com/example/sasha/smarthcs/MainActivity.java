ff84c482e38758db2e418f07baa0822a38588181
GerONSo <sergon2002@gmail.com>
2018-09-29 20:38:36 +0300

    Merge branch 'master' of C:\Users\glebi\SmartHCS with conflicts.

--- app/src/main/java/com/example/sasha/smarthcs/MainActivity.java
+++ app/src/main/java/com/example/sasha/smarthcs/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,11 +27,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button loginButton = findViewById(R.id.login_button);
+        final EditText loginText = findViewById(R.id.login_text);
+        EditText passwordText = findViewById(R.id.password_text);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String cur_name = loginText.getText().toString();
+                String cur_password = loginText.getText().toString();
+                ArrayList<User> users = ProfileActivity.user_base;
+                for(int i = 0; i < users.size(); i++) {
+                    if(cur_name.equals(users.get(i).getLogin()) && cur_password.equals(users.get(i).getPassword())) {
+                        openProfile();
+                    }
+                }
+            }
+        });
     }
 
-    public void gonext(View view)
-    {
-        Intent intent = new Intent(this, ProfileActivity.class);
-        startActivity(intent);
-    }
+//    void openProfile() {
+//        Intent intent = new Intent(this, ProfileActivity.class);
+//        startActivity(intent);
+//    }
 }
--- app/src/main/java/com/example/sasha/smarthcs/User.java
+++ app/src/main/java/com/example/sasha/smarthcs/User.java
@@ -3,6 +3,14 @@ package com.example.sasha.smarthcs;
 import java.util.ArrayList;
 
 public class User {
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
     String login = "", password = "";
     ArrayList<Bill> history;
     public void set_login(String LOGIN)
@@ -12,5 +20,13 @@ public class User {
     public void set_password(String PASSWORD)
     {
         this.password = PASSWORD;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
     }
 }
